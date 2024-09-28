60f7e0c1c908e7963f784886f6607c77c7b5efff
MrBiscuit <122865263+MrBiscuit921@users.noreply.github.com>
2024-09-28 16:39:25 +1200

    added batch file

---
+++ Install_Spicetify_Stats.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Download the zip file
+powershell -Command "iwr -useb 'https://github.com/harbassan/spicetify-apps/releases/download/stats-v1.1.1/spicetify-stats.release.zip' -OutFile 'spicetify-stats.release.zip'"
+
+:: Extract the zip file to the CustomApps folder
+powershell -Command "Expand-Archive -Path 'spicetify-stats.release.zip' -DestinationPath $env:appdata\spicetify\CustomApps -Force"
+
+:: make suke spicetify notices change
+powershell -Command "spicetify config custom_apps stats"
+powershell -Command "spicetify apply"
+
+:: Notify user of completion
+echo Download and extraction complete.
+pause
